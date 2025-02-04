2e5e93f4536891ff94e0cb3f8d8b57cfd45be148
Sebastian <sebastian.alfers@gmail.com>
2025-02-04 16:17:51 +0100

    two cases that both should work

--- akka-persistence-typed-tests/src/test/scala/akka/persistence/typed/scaladsl/EventSourcedSequenceNumberBasicSpec.scala
+++ akka-persistence-typed-tests/src/test/scala/akka/persistence/typed/scaladsl/EventSourcedSequenceNumberBasicSpec.scala
@@ -6,14 +6,13 @@ package akka.persistence.typed.scaladsl
 
 import com.typesafe.config.ConfigFactory
 import org.scalatest.wordspec.AnyWordSpecLike
-
 import akka.actor.testkit.typed.scaladsl.LogCapturing
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import akka.actor.testkit.typed.scaladsl.TestProbe
 import akka.actor.typed.ActorRef
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
-import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.{ PersistenceId, RecoveryCompleted }
 
 //import akka.persistence.typed.RecoveryCompleted
 
@@ -33,33 +32,40 @@ class EventSourcedSequenceNumberBasicSpec
     with AnyWordSpecLike
     with LogCapturing {
 
-  private def behavior(pid: PersistenceId, probe: ActorRef[String]): Behavior[String] =
-    Behaviors.setup(
-      ctx =>
-        EventSourcedBehavior[String, String, String](
-          pid,
-          "", { (_, _) =>
-            println(s"command: ******* ${EventSourcedBehavior.lastSequenceNumber(ctx)}")
-            probe ! s"${EventSourcedBehavior.lastSequenceNumber(ctx)} command"
-            Effect.persist("event")
-          }, { (_, evt) =>
-            println(s"event: ******* ${EventSourcedBehavior.lastSequenceNumber(ctx)}")
-            probe ! s"${EventSourcedBehavior.lastSequenceNumber(ctx)} event"
-            evt
-          })
-//        .receiveSignal {
-//        case (_, RecoveryCompleted) =>
-//          probe ! s"${EventSourcedBehavior.lastSequenceNumber(ctx)} onRecoveryComplete"
-//      }
-    )
+  private def behavior(
+      pid: PersistenceId,
+      probe: ActorRef[String],
+      sendOnRecoveryCompleted: Boolean): Behavior[String] =
+    Behaviors.setup(ctx =>
+      EventSourcedBehavior[String, String, String](pid, "", { (_, _) =>
+        val nr = EventSourcedBehavior.lastSequenceNumber(ctx)
+        println(s"command: ******* $nr")
+        probe ! s"$nr command"
+        Effect.persist("event")
+      }, { (_, evt) =>
+        val nr = EventSourcedBehavior.lastSequenceNumber(ctx)
+        println(s"event: ******* $nr")
+        probe ! s"$nr event"
+        evt
+      }).receiveSignal {
+        case (_, RecoveryCompleted) =>
+          if (sendOnRecoveryCompleted) {
+            val nr = EventSourcedBehavior.lastSequenceNumber(ctx)
+            println(s"recov: ******* $nr")
+            probe ! s"${EventSourcedBehavior.lastSequenceNumber(ctx)} onRecoveryComplete"
+          }
+      })
 
   "The sequence number" must {
 
-    "be accessible in the handlers" in {
+    def runTheSpec(testProbe: Boolean) = {
       val probe = TestProbe[String]()
-      val ref = spawn(behavior(PersistenceId.ofUniqueId("ess-1"), probe.ref))
 
-      //probe.expectMessage("0 onRecoveryComplete")
+      val ref = spawn(behavior(PersistenceId.ofUniqueId("ess-1"), probe.ref, testProbe))
+
+      if (testProbe) {
+        probe.expectMessage("0 onRecoveryComplete")
+      }
 
       ref ! "cmd"
       probe.expectMessage("0 command")
@@ -68,6 +74,14 @@ class EventSourcedSequenceNumberBasicSpec
       ref ! "cmd"
       probe.expectMessage("1 command")
       probe.expectMessage("2 event")
+    }
+
+    "work" in {
+      runTheSpec(testProbe = true)
+    }
+
+    "also work" in {
+      runTheSpec(testProbe = false)
     }
   }
 }
